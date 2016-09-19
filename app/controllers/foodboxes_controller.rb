21cfd961e12b0397556e7f14d3b48c32076aa3be
Ayaka Mihira <mihiaya.smile@gmail.com>
2016-09-19 18:48:55 +0900

    user_idが届くように変更 マイページ作成のためのusers#showルーティング追加

--- app/controllers/foodboxes_controller.rb
+++ app/controllers/foodboxes_controller.rb
@@ -30,7 +30,7 @@ class FoodboxesController < ApplicationController
 
   private
   def foodbox_params
-    params.permit(:photo, :restaurant, :comment, :user_id)
+    params.require(:foodbox).permit(:photo, :restaurant, :comment).merge(user_id: current_user.id)
   end
 
   def move_to_index
