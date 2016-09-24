85dc7cbf5a5a24a5786ab83c08e71aa390c5a277
Ayaka Mihira <mihiaya.smile@gmail.com>
2016-09-24 14:22:01 +0900

    add user seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
+User.create(email: 'mihiaya@gmail.com', password: 'mihiaya0614', nickname: 'mihi')# This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
 # Examples:
